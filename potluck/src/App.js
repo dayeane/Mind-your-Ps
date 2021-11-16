794b095c78871973a675fc3f2a4181547ab0924c
Dayeane <dayeane85@gmail.com>
2021-11-15 20:25:37 -0800

    Guest Component with filter

--- potluck/src/App.js
+++ potluck/src/App.js
@@ -1,10 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar';
 import HomeScreen from './HomeScreen';
 import EventInfo from './EventInfo';
+import GuestContainer from './GuestContainer';
 
 function App() {
+
+  const event = {
+    guests: [
+      {
+        id: 1,
+        name: "Dayeane Cortes",
+        email: "dayis@example.com",
+        phone: "5554312244",
+        confirm: "false"
+      },
+      {
+        id: 2,
+        name: "Dan LeFever",
+        email: "dan@example.com",
+        phone: "5554312245",
+        confirm: "true"
+      },
+      {
+        id: 3,
+        name: "David Avery",
+        email: "david@example.com",
+        phone: "5554312249",
+        confirm: "false"
+      },
+    ]
+  }
+
   return (
     <div className="App">
       <h1>POTLUCK</h1>
@@ -12,6 +40,7 @@ function App() {
       <NavBar />
       <HomeScreen />
       <EventInfo />
+      <GuestContainer guests={event.guests}/>
     </div>
   );
 }
