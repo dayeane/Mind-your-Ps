cd9db7d3336002dc17b46ebf2e5f3abef788cf3c
Dan <dt.lefever@gmail.com>
2021-11-16 09:49:23 -0700

    deleted unused files

--- potluck/src/AddItem.js
+++ potluck/src/AddItem.js
@@ -2,14 +2,21 @@ import react from "react"
 
 function AddItem () {
     return (
+      <>
         <h2>Here is a form to add an item</h2>
         <div>
             <form>
-            <div>
-                
-            </div>
+                <div>
+                    <input type="text" name="item">Item Needed</input>
+                    <input type="text" name="category">Category</input>
+                    <input type="text" name="claimer">Who's Bring It</input>
+                </div>
+                <button type="submit">
+                    Add Item
+                </button>
             </form>
         </div>
+      </>
     )
 }
 
