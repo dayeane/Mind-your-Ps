65ca86daf7ee22308e8c4d5954a9c47d9fc68e2d
Dayeane <dayeane85@gmail.com>
2021-11-18 17:54:49 -0800

    mire style

--- potluck/src/Photos.js
+++ potluck/src/Photos.js
@@ -5,17 +5,19 @@ function Photos({eventId, allEventPhotos, setAllEventPhotos}) {
 console.log(eventId)
     if (!allEventPhotos){ return <h2>..Please select and event from home page</h2> }
   
-    const photoList = allEventPhotos.map((photo)=> <img key={photo.id}src={photo.url} alt={photo.name} width="300" height="200" /> )
+    const photoList = allEventPhotos.map((photo)=> <img className="m-2" key={photo.id}src={photo.url} alt={photo.name} width="300" height="200" /> )
 
    
     return (
     <>
-        <h1>Best Memories</h1>
+      <div className="card row mt-5">
+        <div className="card-header"><h2>Best Memories</h2></div>
         <CreateImage eventId={eventId} setAllEventPhotos={setAllEventPhotos} allEventPhotos={allEventPhotos} />
-        <div>
-          {photoList}
-        </div>
-  </>
+      </div>
+      <div className="mt-5">
+        {photoList}
+      </div>
+    </>
     )
   };
 
