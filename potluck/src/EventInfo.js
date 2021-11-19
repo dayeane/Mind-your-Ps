fc10ff7d31c2bb747bff8ba3765210a70719a42f
David Avery <eismausen@gmail.com>
2021-11-19 10:05:20 -0800

    Duplicate display fix

--- potluck/src/EventInfo.js
+++ potluck/src/EventInfo.js
@@ -15,16 +15,6 @@ function EventInfo({eventObj}) {
     return (
         <div className="card mt-5" id="eventInfo">
             <div className="card-header"><h2>EVENT INFO</h2></div>
-            <h4>Name: {name}</h4>
-            <p>Event code: {code}</p> 
-            <p>Description: {description}</p>
-            <p>Theme: {theme}</p>
-            <p>Location: {location}</p>
-            <p>Date: {date}</p>
-            <p>Time: {time}</p>
-            <p>Invite: {inviteStructure}</p>
-            <p>Dress code: {dressCode}</p>
-            <button className="btn btn-primary row col-1 align-self-center" onClick={() => history.push('/editEvent')}>Edit Event</button>
             <h4> <strong>Name</strong>: {name}</h4>
             <p> <strong>Event code</strong>: {code}</p> 
             <p> <strong>Description</strong>: {description}</p>
@@ -34,6 +24,7 @@ function EventInfo({eventObj}) {
             <p> <strong>Time</strong>: {time}</p>
             <p> <strong>Invite</strong>: {inviteStructure}</p>
             <p> <strong>Dress code</strong>: {dressCode}</p>
+            <button className="btn btn-primary row col-1 align-self-center" onClick={() => history.push('/editEvent')}>Edit Event</button>      
         </div>
     ) 
 }
